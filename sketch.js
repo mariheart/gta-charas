5a00a22522bcc28d9a50659650f8e648cc53e35e
Maru-chan <mariheart02@gmail.com>
2020-04-14 00:57:32 -0400

    Add files via upload
    hopefully this looks good

---
+++ sketch.js
@@ -0,0 +1,209 @@
+let gtaDatabase =
+[
+    { //GTA I
+        "name": "Unknown",
+        "game": "GTA I",
+        "city": "Liberty City",
+        "color": "#FDB813", // cab yellow
+        "text_color": "#000000", //black
+        "picture": "https://vignette.wikia.nocookie.net/gtawiki/images/8/86/Protagonists-GTA1.png/revision/latest?cb=20190902184308",
+        "image_song": "Theme from GTA I",
+    },
+
+    { //GTA London 1969
+        "name": "Unknown",
+        "game": "GTA London 1969",
+        "city": "London",
+        "color": "001f7e", // union blue
+        "text_color": "#d00c27", // union red
+        "picture": "https://vignette.wikia.nocookie.net/gtawiki/images/2/21/Protagonists-GTALondon.png/revision/latest?cb=20190902190840",
+        "image_song": "GTA London 1969 Main Theme",
+
+    },
+
+    { //GTA II
+        "name": "Claude Speed",
+        "game": "GTA II",
+        "city": "Liberty City",
+        "color": "585858", // dark grey
+        "text_color": "#1b2e33", //city blue
+        "picture": "https://a1-images.myspacecdn.com/images03/31/f51d01aa7e794c88aa701354aeca8bf2/600x600.jpg",
+        "image_song": "Short Change - E-Z Rollers",
+    },
+
+    { //GTA III
+        "name": "Claude",
+        "game": "GTA III",
+        "city": "Liberty City",
+        "color": " 	#D3D3D3", // light grey
+        "text_color": "#2c6a7a", //city blue
+        "picture": "https://vignette.wikia.nocookie.net/gtawiki/images/7/7b/Claude-GTAIII-Artwork.jpg/revision/latest/top-crop/width/300/height/300?cb=20110414121341",
+        "image_song": "Theme from GTA III",
+    },
+
+
+    { //GTA Vice City
+        "name": "Tommy Vercetti",
+        "game": "GTA Vice City",
+        "city": "Vice City",
+        "color": "#c071fe", // neon purple
+        "text_color": "#ffffff", //white
+        "picture": "https://vignette.wikia.nocookie.net/evil/images/d/d1/Tommy_Vercetti_Artwork.jpg/revision/latest?cb=20181107195455&path-prefix=pl",
+        "image_song": "Theme from Vice City",
+    },
+
+    { //GTA San Andreas
+        "name": "Carl Johnson (C.J.)",
+        "game": "GTA San Andreas",
+        "city": "Los Santos",
+        "color": "#86a17d", // muted green
+        "text_color": "#1c300c", //grassy green
+        "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTcFijFtCL4mOQR4GmXBtlj-jxCmtTaPFdmbYdtzzfVUvLBqq9M&usqp=CAU",
+        "image_song": "Theme from San Andreas",
+    },
+
+    { //GTA Liberty City Stories
+        "name": "Toni Cipriani",
+        "game": "GTA Liberty City Stories",
+        "city": "Liberty City",
+        "color": "#303030", // dull grey
+        "text_color": "#cc443f", //muted red
+        "picture": "https://vignette.wikia.nocookie.net/gtawiki/images/1/14/Gta3-anniversary_toni_1600x1200.jpg/revision/latest?cb=20120611155150",
+        "image_song": "A Dark March - Danger Mouse",
+    },
+
+    { //GTA Vice City Stories
+        "name": "Victor Vance",
+        "game": "GTA Vice City Stories",
+        "city": "Vice City",
+        "color": "#fe46a5", // neon pink
+        "text_color": "#730bdb", //neon purple
+        "picture": "https://img4.wikia.nocookie.net/__cb20100703232947/es.gta/images/1/19/Vic_Art.png",
+        "image_song": "Theme from Vice City Stories",
+    },
+
+    { //GTA IV
+        "name": "Niko Bellic",
+        "game": "GTA IV",
+        "city": "Liberty City",
+        "color": "#a2c3d0", // sky blue
+        "text_color": "#264145", //murky blue
+        "picture": "https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Niko_Bellic.jpg/220px-Niko_Bellic.jpg",
+        "image_song": "Soviet Connection",
+    },
+
+    { //GTA The Lost and Damned
+        "name": "Johnny Klebitz",
+        "game": "GTA IV: The Lost and Damned",
+        "city": "Liberty City",
+        "color": "#8a0303", // blood red
+        "text_color": "#cbd0d6", //steel grey
+        "picture": "https://i.pinimg.com/originals/8d/a6/44/8da644e53f22949a33946954ac46cce6.jpg",
+        "image_song": "Theme from The Lost and Damned",
+    },
+
+    { //GTA The Ballad of Gay Tony
+        "name": "Luis Lopez",
+        "game": "GTA IV: The Ballad of Gay Tony",
+        "city": "Liberty City",
+        "color": "#b19cd9", // pastel purple
+        "text_color": "#e9e6fa", //light pastel purple
+        "picture": "https://i.pinimg.com/originals/d6/31/99/d6319912b9ea4f6c71bcc47e6e060498.jpg",
+        "image_song": "Pjanoo - Eric Prydz",
+    },
+
+    { //GTA Online 
+        "name": "Unknown",
+        "game": "GTA Online",
+        "city": "Los Santos",
+        "color": "#bfdef5", // city blue
+        "text_color": "#383e42", // city grey
+        "picture": "https://www.kindpng.com/picc/m/590-5904054_gta-png-gta-v-characters-png-transparent-png.png",
+        "image_song": "Welcome to Los Santos - Oh No",
+    },
+
+    { //GTA V - Michael
+        "name": "Michael de Santa",
+        "game": "GTA V",
+        "city": "Los Santos",
+        "color": "#80d7ff", // cloud blue
+        "text_color": "#2199ff", //blue
+        "picture": "https://cdn.staticneo.com/w/gta5/thumb/f/f3/Michael.png/280px-Michael.png",
+        "image_song": "Radio Ga Ga - Queen",
+    },
+
+    { //GTA V - Franklin
+        "name": "Franklin Clinton",
+        "game": "GTA V",
+        "city": "Los Santos",
+        "color": "#b3ffe0", // pastel green
+        "text_color": "#1eba98", //green
+        "picture": "https://images.eurogamer.net/2013/articles//a/1/6/1/2/8/6/2/eurogamer-nv8krx.jpg/EG11/resize/300x-1/quality/80/format/jpg",
+        "image_song": "Hood Gone Love It - Jay Rock ft. Kendrick Lamar",
+    },
+
+    { //GTA V - Trevor
+        "name": "Trevor Phillips",
+        "game": "GTA V",
+        "city": "Los Santos",
+        "color": "#ffa86e", // fire orange
+        "text_color": "#e8541a", //orange
+        "picture": "https://i.pinimg.com/originals/c7/fa/f1/c7faf1836cafe34f9d178c67fbcda4eb.jpg",
+        "image_song": "Are You Sure Hank Done It This Way - Waylon Jennings",
+    },
+]
+console.log(gtaDatabase)
+
+let element = document.getElementById("element");
+let paragraph = document.getElementById("paragraph");
+let contentGrid = document.getElementById("contentGrid");
+
+
+for (var i = 0; i < gtaDatabase.length; i++) {
+    createElement(gtaDatabase[i]);
+}
+
+
+function createElement(jsonInput) {
+    //Set background color and create item div!
+    let newContentElement = document.createElement("DIV");
+    newContentElement.style.backgroundColor = jsonInput['color'];
+    newContentElement.style.color = jsonInput['text_color'];
+    newContentElement.classList.add('contentItem');
+
+    //Set name
+    let newContentName = document.createElement("H2");
+    newContentName.classList.add('contentName');
+    newContentName.innerHTML = jsonInput['name'];
+    newContentElement.appendChild(newContentName);
+
+    //Set game
+    let newContentGame = document.createElement("H3");
+    newContentGame.classList.add('contentGame');
+    newContentGame.innerHTML = "Game: " + jsonInput['game'];
+    newContentElement.appendChild(newContentGame);
+
+    //Set city
+    let newContentCity = document.createElement("H3");
+    newContentCity.classList.add('contentCity');
+    newContentCity.innerHTML = "City: " + jsonInput['city'];
+    newContentElement.appendChild(newContentCity);
+
+    //create image
+    let newImage = document.createElement("IMG");
+    newImage.classList.add("key_img");
+    newImage.src = jsonInput['picture'];
+    newContentElement.appendChild(newImage);
+
+    //Set image song
+    let newContentSong = document.createElement("H3");
+    newContentSong.classList.add('contentSong');
+    newContentSong.innerHTML = "Image Song: " + jsonInput['image_song'];
+    newContentElement.appendChild(newContentSong);
+
+
+    //add stuff to page
+    contentGrid.appendChild(newContentElement);
+
+}
+
